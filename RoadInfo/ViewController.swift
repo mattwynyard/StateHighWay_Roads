95969b3848c8ac12534cec226737cecaeb5d3ef8
Matt Wynyard <mjwynyard@gmail.com>
2015-11-05 13:53:08 +1300

    Initial Commit

---
+++ RoadInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadInfo
+//
+//  Created by Matt Wynyard on 5/11/15.
+//  Copyright © 2015 Niobium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
